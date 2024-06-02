b37a2b75163792cf75318a9834d688eda3a80fd3
motianshi <motianshi@qq.com>
2024-06-02 15:11:06 +0800

    kafka producer consumer init

--- demo-kafka/src/main/java/com/anqi/demo/consumer/DemoConsumer.java
+++ demo-kafka/src/main/java/com/anqi/demo/consumer/DemoConsumer.java
@@ -1,30 +1,93 @@
 package com.anqi.demo.consumer;
 
-import com.anqi.demo.common.Constants;
 import com.anqi.demo.producer.DemoProducer;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.Acknowledgment;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
+
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
+import java.time.Duration;
+import java.util.*;
 
 @Component
 public class DemoConsumer {
 
     private static final Logger LOG = LoggerFactory.getLogger(DemoProducer.class);
 
-    @KafkaListener(topics = Constants.DEMO_TOPIC, groupId = Constants.DEMO_TOPIC_GROUP1)
-    public void topic_test(ConsumerRecord<?, ?> record, Acknowledgment ack, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
-        Optional message = Optional.ofNullable(record.value());
-        if (message.isPresent()) {
-            Object msg = message.get();
-            LOG.info("topic.group1 消费了： Topic:" + topic + ",Message:" + msg);
-            ack.acknowledge();
+    private static final String CONSUMER_GROUP_NAME = "CONSUMER_GROUP_NAME";
+    private static final String TOPIC_NAME = "TOPIC_NAME";
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:8080");
+        // 消费分组名
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, CONSUMER_GROUP_NAME);
+        // 是否自动提交offset，默认就是true（拉取完消息后提交偏移量offset 下次就从提交的offset后面开始拉取数据 但是一般不会设置为true 因为会存在丢数据问题 比如在设置的时间（比如一秒）内拉取了10条数据 但是对这十条数据的业务逻辑处理要五秒 然而就在这个处理时间内宕机了 数据没有处理完 但是offset以及提交了 就消费不到丢失的数据了 在比如这十条数据处理时间只要0.1秒 我没有有提交offset就宕机了 也会丢数据  而设置false就不会自动提交offset 如果不手动提交 下次启动还会再次消费）
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        // 自动提交offset的间隔时间
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        //props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        /*
+           当消费主题的是一个新的消费组，或者指定offset的消费方式，offset不存在，那么应该如何消费
+           latest(默认) ：只消费自己启动之后发送到主题的消息
+           earliest：第一次从头开始消费，以后按照消费offset记录继续消费，这个需要区别于consumer.seekToBeginning(每次都从头开始消费)
+        */
+        //props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        /*
+            consumer给broker发送心跳的间隔时间，broker接收到心跳如果此时有rebalance发生会通过心跳响应将,rebalance方案下发给consumer，这个时间可以稍微短一点
+         */
+        props.put(ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG, 1000);
+        // 服务端broker多久感知不到一个consumer心跳就认为他故障了，会将其踢出消费组，对应的Partition也会被重新分配给其他consumer，默认是10秒
+         props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 10 * 1000);
+        //一次poll最大拉取消息的条数，如果消费者处理速度很快，可以设置大点，如果处理速度一般，可以设置小点
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 500);
+        /*
+            如果两次poll操作间隔超过了这个时间，broker就会认为这个consumer处理能力太弱，会将其踢出消费组，将分区分配给别的consumer消费
+        */
+        props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, 30 * 1000);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singletonList(TOPIC_NAME));
+        // 消费指定分区
+        //consumer.assign(Arrays.asList(new TopicPartition(TOPIC_NAME, 0)));
+        //消息回溯消费
+        //consumer.seekToBeginning(Arrays.asList(new TopicPartition(TOPIC_NAME, 0)));
+        //指定offset消费
+        //consumer.seek(new TopicPartition(TOPIC_NAME, 0), 10);
+        //从指定时间点开始消费
+        //List<PartitionInfo> topicPartitions = consumer.partitionsFor(TOPIC_NAME);
+        int count = 0;
+        try {
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
+                for (ConsumerRecord<String, String> r : records) {
+                    String key = r.key();
+                    String value = r.value();
+                    Map<TopicPartition, OffsetAndMetadata> offsets = new HashMap<>();
+                    TopicPartition topicPartition = new TopicPartition(r.topic(), r.partition());
+                    //消费者提交的消费位移=当前消费消息的位移+1
+                    OffsetAndMetadata metadata = new OffsetAndMetadata(r.offset() + 1);
+                    offsets.put(topicPartition, metadata);
+                    if(count % 1000 == 0){
+                        consumer.commitSync(offsets);
+                    }
+                    count++;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                // 同步提交位移
+                consumer.commitSync();
+            }finally{
+                consumer.close();
+            }
         }
+
     }
 }
--- demo-kafka/src/main/java/com/anqi/demo/producer/DemoProducer.java
+++ demo-kafka/src/main/java/com/anqi/demo/producer/DemoProducer.java
@@ -2,40 +2,60 @@ package com.anqi.demo.producer;
 
 
 import com.anqi.demo.common.Constants;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
+
 import org.springframework.stereotype.Component;
-import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.util.concurrent.ListenableFutureCallback;
+
 
 import java.util.Date;
+import java.util.Properties;
 
 @Component
 public class DemoProducer {
 
-    @Autowired
-    private KafkaTemplate<String, String> template;
+
 
     private static final Logger LOG = LoggerFactory.getLogger(DemoProducer.class);
 
 
-    public void send(String val) {
-        ListenableFuture<SendResult<String, String>> res = template.send(Constants.DEMO_TOPIC, val);
-        LOG.info("kafka send msg{}", new Date());
-        res.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
-            @Override
-            public void onFailure(Throwable throwable) {
-                LOG.error("kafka send error");
-                //todo
-            }
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:2122");
+        /*
+             发出消息持久化机制参数
+            （1）acks=0： 表示producer不需要等待任何broker确认收到消息的回复，就可以继续发送下一条消息。性能最高，但是最容易丢消息。
+            （2）acks=1： 至少要等待leader已经成功将数据写入本地log，但是不需要等待所有follower是否成功写入。就可以继续发送下一条消息。这种情况下，
+             如果follower没有成功备份数据，而此时leader又挂掉，则消息会丢失。
+            （3）acks=‐1或all： 需要等待min.insync.replicas(默认为1，推荐配置大于等于2) 这个参数配置的副本个数都成功写入日志，这种策略会保证只要
+             有一个备份存活就不会丢失数据。这是最强的数据保证。一般除非是金融级别，或跟钱打交道的场景才会使用这种配置。
+        */
+        props.put(ProducerConfig.ACKS_CONFIG, "1");
+        //发送失败会重试，默认重试间隔100ms，重试能保证消息发送的可靠性，但是也可能造成消息重复发送，比如网络抖动，所以需要在接收者那边做好消息接收的幂等性处理
+        props.put(ProducerConfig.RETRIES_CONFIG, 3);
+        //重试间隔设置
+        props.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, 300);
+        //设置发送消息的本地缓冲区，如果设置了该缓冲区，消息会先发送到本地缓冲区，可以提高消息发送性能，默认值是33554432，即32MB
+        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+        //kafka本地线程会从缓冲区取数据，批量发送到broker，设置批量发送消息的大小，默认值是16384，即16kb，就是说一个batch满了16kb就发送出去
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+        /*
+           默认值是0，意思就是消息必须立即被发送，但这样会影响性能。一般设置10毫秒左右，就是说这个消息发送完后会进入本地的一个batch，如果10毫秒内，这个batch满
+           了16kb就会随batch一起被发送出去，如果10毫秒内，batch没满，那么也必须把消息发送出去，不能让消息的发送延迟时间太长
+         */
+        props.put(ProducerConfig.LINGER_MS_CONFIG, 10);
+        //把发送的key从字符串序列化为字节数组
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        //把发送消息value从字符串序列化为字节数组
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        producer.abortTransaction();;
 
-            @Override
-            public void onSuccess(SendResult<String, String> stringStringSendResult) {
-                LOG.info("kafka send success");
-            }
-        });
     }
+
+
 }
